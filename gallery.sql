2448d67e83a2322fe784dccd2ae17000dd47ce0e
Matej Malcic <matej.malcic3@gmail.com>
2020-04-19 21:13:42 +0200

    set tables in database

--- gallery.sql
+++ gallery.sql
@@ -3,8 +3,18 @@ create database gallery default character set utf8;
 use gallery;
 
 CREATE TABLE users (
-id		int not null primary key auto_increment,
-nick    varchar(255),
-name    varchar(100)
-
+id			int not null primary key auto_increment,
+nick   		varchar(255) not null,
+name    	varchar(100) not null,
+email		varchar(50) not null,
+password    char(60) not null,
+adress		varchar(100)
 );
+
+create table photos(
+id		int not null primary key auto_increment,
+user	int not null,
+photo	varchar(255)
+);
+
+alter table photos add foreign key (user) references users(id);
